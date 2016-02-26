5b17156bec1f8c4159a51222e48c2c1a0a67ef82
bayudirsa <bayudirsa@outlook.co.id>
2016-02-26 08:54:20 -0800

    update data

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,4 @@
 <?php
-mysql_connect("localhost","root","");
+mysql_connect("bosh.database.windows.net","bayudirsa","01July1gg7");
 mysql_select_db("sistem_pln") or die ("Koneksi ke DataBase GAGAL..!! : ".mysql_error());
 ?>
